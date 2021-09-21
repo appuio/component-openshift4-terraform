f034b34f229ac695bd08cce6110007b89e351a2e
Sebastian Widmer <sebastian.widmer@vshn.net>
2021-09-21 11:02:05 +0200

    Update cloudscale.ch terraform module to v3.0.0

---
+++ scripts/migrate-state-v2-v3.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+
+terraform state mv module.cluster.cloudscale_floating_ip.api_vip module.cluster.module.lb.cloudscale_floating_ip.api_vip
+terraform state mv module.cluster.cloudscale_floating_ip.router_vip module.cluster.module.lb.cloudscale_floating_ip.router_vip
+terraform state mv module.cluster.cloudscale_floating_ip.nat_vip module.cluster.module.lb.cloudscale_floating_ip.nat_vip
+
+terraform state mv module.cluster.cloudscale_server.lb module.cluster.module.lb.cloudscale_server.lb
+terraform state mv module.cluster.cloudscale_server_group.lb module.cluster.module.lb.cloudscale_server_group.lb
+terraform state mv module.cluster.random_id.lb module.cluster.module.lb.random_id.lb
+terraform state mv module.cluster.null_resource.register_lb module.cluster.module.lb.null_resource.register_lb
+
+terraform state mv module.cluster.data.local_file.hieradata_mr_url 'module.cluster.module.lb.module.hiera.data.local_file.hieradata_mr_url[0]'
+terraform state mv 'module.cluster.gitfile_checkout.appuio_hieradata[0]' module.cluster.module.lb.module.hiera.gitfile_checkout.appuio_hieradata
+terraform state mv 'module.cluster.local_file.lb_hieradata[0]' module.cluster.module.lb.module.hiera.local_file.lb_hieradata
